6fbc536e4629ca561e4f994ff6433ad4060b915a
a.kolomeytsev <akolomeytsev@vivid.money>
2020-10-17 16:55:08 +0300

    Второе задание выполнено

--- src/chapter_2.rs
+++ src/chapter_2.rs
@@ -1,6 +1,7 @@
 
+/// Сортировка вставками (2.1.1)
 pub fn insertion_sort(arr: &mut Vec<i32>) {
-    for j in 2..arr.len() {
+    for j in 1..arr.len() {
         let key = arr[j];
         let mut i = j - 1;
         while i > 0 && arr[i] > key {
@@ -8,5 +9,18 @@ pub fn insertion_sort(arr: &mut Vec<i32>) {
             i -= 1;
         }
         if arr[0] > key { arr.swap(1, 0) }
+    }
+}
+
+/// Сортировка вставками в убывающем порядке (2.1.2)
+pub fn insertion_sort_descending(arr: &mut Vec<i32>) {
+    for j in 1..arr.len() {
+        let key = arr[j];
+        let mut i = j - 1;
+        while i > 0 && arr[i] < key {
+            arr.swap(i + 1, i);
+            i -= 1;
+        }
+        if arr[0] < key { arr.swap(1, 0) }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,16 @@ fn main() {}
 
 #[test]
 fn insertion_sort_test() {
-    let mut actual: Vec<i32> = vec![1, 9, 5, 4, 3, 7, 8, 6, 2, 0, 15, 999, -1000];
-    let expected: Vec<i32> = vec![-1000, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 999];
+    let mut actual: Vec<i32> = vec![31, 41, 59, 26, 41, 58];
+    let expected: Vec<i32> = vec![26, 31, 41, 41, 58, 59];
     chapter_2::insertion_sort(&mut actual);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn insertion_sort_descending_test() {
+    let mut actual: Vec<i32> = vec![31, 41, 59, 26, 41, 58];
+    let expected: Vec<i32> = vec![59, 58, 41, 41, 31, 26];
+    chapter_2::insertion_sort_descending(&mut actual);
     assert_eq!(actual, expected);
 }
